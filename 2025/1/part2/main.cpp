f0844a8910b254f0f4c9f7def47cffd7a9df4fa0
axdelafuen <axel.dlf63@gmail.com>
2025-12-01 18:27:01 +0000

    solve part2

--- 2025/1/part2/main.cpp
+++ 2025/1/part2/main.cpp
@@ -20,26 +20,28 @@ int main() {
         int amount = std::stoi(l.substr(1));
         if (l[0] == 'L') {
             dial -= amount;
-            if (dial < 0)
-                result++;
-            while (dial < 0) {
-                dial += 100;
+            if (dial < 0) {
+                while (dial < 0) {
+                    dial+=100;    
+                    result++;
+                }
             }
         }
         if (l[0] == 'R') {
             dial += amount;
-            while (dial > 99) {
-                dial -= 100;
+            if (dial > 99) {
+                while (dial > 99) {
+                    dial-=100;
+                    result++;
+                }
             }
         }
 
         std::cout << "DEBUG step (" << l << "): " << dial << std::endl;
-
-        if (dial == 0)
-            result++;
     }
 
     std::cout << "Result: " << result << std::endl;
 
     return 0;
 }
+
